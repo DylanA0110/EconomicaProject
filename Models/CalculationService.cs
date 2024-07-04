54bebe285378e4131789536e01ce87ce44985a15
DylanA0110 <dylanjo0606@gmail.com>
2024-07-04 10:33:37 -0600

    Agregando comentarios

--- Models/CalculationService.cs
+++ Models/CalculationService.cs
@@ -1,90 +1,140 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Models
 {
     public class CalculationService : ICalculationService
     {
+        // Interés Simple
+
         public decimal CalcularISValorFuturo(Interes interes)
         {
+            // Fórmula: VF = VP * (1 + i * n)
             return interes.Presente * (1 + (interes.TasaInteres / 100) * interes.Periodo);
         }
 
         public decimal CalcularISValorPresente(Interes interes)
         {
+            // Fórmula: VP = VF / (1 + i * n)
             return interes.Futuro / (1 + (interes.TasaInteres / 100) * interes.Periodo);
         }
 
         public decimal CalcularISInteres(Interes interes)
         {
+            // Fórmula: I = VP * i * n
             return interes.Presente * (interes.TasaInteres / 100) * interes.Periodo;
         }
 
         public decimal CalcularISTPeriodo(Interes interes)
         {
+            // Fórmula: n = (VF / VP - 1) / i
             return (interes.Futuro / interes.Presente - 1) / (interes.TasaInteres / 100);
         }
 
+        // Interés Compuesto
+
         public decimal CalcularICValorFuturo(Interes interes)
         {
+            // Fórmula: VF = VP * (1 + i)^n
             return interes.Presente * (decimal)Math.Pow((double)(1 + (interes.TasaInteres / 100)), (double)interes.Periodo);
         }
 
         public decimal CalcularICValorPresente(Interes interes)
         {
+            // Fórmula: VP = VF / (1 + i)^n
             return interes.Futuro / (decimal)Math.Pow((double)(1 + (interes.TasaInteres / 100)), (double)interes.Periodo);
         }
 
         public decimal CalcularICInteres(Interes interes)
         {
+            // Fórmula: I = VF - VP
             return CalcularICValorFuturo(interes) - interes.Presente;
         }
 
         public decimal CalcularICTPeriodo(Interes interes)
         {
+            // Fórmula: n = log(VF / VP) / log(1 + i)
             return (decimal)(Math.Log((double)(interes.Futuro / interes.Presente)) / Math.Log((double)(1 + (interes.TasaInteres / 100))));
         }
 
+        // Anualidades Vencidas y Anticipadas
+
         public decimal AnualidadVencidaVP(Anualidad anualidad)
         {
-            return anualidad.Monto * (1 - (decimal)Math.Pow((double)(1 + anualidad.TasaInteres / 100), (double)-anualidad.Periodos)) / (anualidad.TasaInteres / 100);
+            // Fórmula: VP = R * [1 - (1 + i)^-n] / i
+            // Tasa de interés periódica (mensual en este caso)
+            decimal tasaInteresPeriodica = anualidad.TasaInteres / 100 / 12;
+
+            // Verificar que no sea cero para evitar división por cero
+            if (tasaInteresPeriodica == 0)
+            {
+                // Si la tasa es 0%, el valor presente es simplemente el monto multiplicado por el número de períodos
+                return anualidad.Monto * anualidad.Periodos;
+            }
+
+            // Fórmula completa: VP = R * [1 - (1 + i)^-n] / i
+            double tasaInteresDouble = (double)tasaInteresPeriodica;
+            double factor = 1 - Math.Pow(1 + tasaInteresDouble, (double)-anualidad.Periodos);
+            return anualidad.Monto * (decimal)(factor / tasaInteresDouble);
         }
 
         public decimal AnualidadVencidaVF(Anualidad anualidad)
         {
-            return anualidad.Monto * ((decimal)Math.Pow((double)(1 + anualidad.TasaInteres / 100), (double)anualidad.Periodos) - 1) / (anualidad.TasaInteres / 100);
+            // Fórmula: VF = R * [(1 + i)^n - 1] / i
+            // Tasa de interés periódica (mensual en este caso)
+            decimal tasaInteresPeriodica = anualidad.TasaInteres / 100 / 12;
+
+            // Verificar que no sea cero para evitar división por cero
+            if (tasaInteresPeriodica == 0)
+            {
+                // Si la tasa es 0%, el valor futuro es simplemente el monto multiplicado por el número de períodos
+                return anualidad.Monto * anualidad.Periodos;
+            }
+
+            // Fórmula completa: VF = R * [(1 + i)^n - 1] / i
+            double tasaInteresDouble = (double)tasaInteresPeriodica;
+            double factor = Math.Pow(1 + tasaInteresDouble, (double)anualidad.Periodos) - 1;
+            return anualidad.Monto * (decimal)(factor / tasaInteresDouble);
         }
 
         public decimal AnualidadAnticipadaVP(Anualidad anualidad)
         {
+            // Fórmula: VP_antic = R * [1 - (1 + i)^-n] / i * (1 + i)
             return AnualidadVencidaVP(anualidad) * (1 + anualidad.TasaInteres / 100);
         }
 
         public decimal AnualidadAnticipadaVF(Anualidad anualidad)
         {
+            // Fórmula: VF_antic = R * [(1 + i)^n - 1] / i * (1 + i)
             return AnualidadVencidaVF(anualidad) * (1 + anualidad.TasaInteres / 100);
         }
 
         public decimal AnualidadDiferidaVP(AnualidadDiferida anualidadDiferida)
         {
+            // Fórmula: VP_diferido = R * [1 - (1 + i)^-n] / i / (1 + i)^d
             return AnualidadVencidaVP(anualidadDiferida) / (decimal)Math.Pow((double)(1 + anualidadDiferida.TasaInteres / 100), (double)anualidadDiferida.PeriodosDiferimiento);
         }
 
         public decimal AnualidadDiferidaVF(AnualidadDiferida anualidadDiferida)
         {
+            // Fórmula: VF_diferido = R * [(1 + i)^n - 1] / i / (1 + i)^d
             return AnualidadVencidaVF(anualidadDiferida) / (decimal)Math.Pow((double)(1 + anualidadDiferida.TasaInteres / 100), (double)anualidadDiferida.PeriodosDiferimiento);
         }
 
+        // Tasa Mínima Aceptable de Rendimiento (TMAR)
+
         public decimal CalcularTMAR(Tmar tmar)
         {
+            // Fórmula: TMAR = TLR + PR
             return tmar.TasaLibreDeRiesgo + tmar.PrimaDeRiesgo;
         }
 
+        // Tasa Interna de Retorno (TIR)
+
         public decimal CalcularTIR(Inversion inversion)
         {
+            // Fórmula: Se usa el método de Newton-Raphson para resolver la ecuación NPV = 0
             Func<decimal, decimal> npv = r => inversion.InversionInicial + inversion.FlujosDeCaja.Select((cashflow, t) => cashflow / (decimal)Math.Pow((double)(1 + r), t + 1)).Sum();
             decimal lowerBound = 0m;
             decimal upperBound = 1m;
@@ -113,13 +163,24 @@ namespace Models
             return lowerBound * 100;
         }
 
+        // Valor Presente Neto (VPN)
+
         public decimal CalcularVPN(Inversion inversion)
         {
+            // Fórmula: VPN = I_0 + Σ(CF_t / (1 + i)^t)
             return inversion.InversionInicial + inversion.FlujosDeCaja.Select((cashflow, t) => cashflow / (decimal)Math.Pow((double)(1 + inversion.TasaDescuento / 100), t + 1)).Sum();
         }
 
+        // Generación de Calendario de Pagos de un Préstamo
+
         public List<Pago> GenerarCalendarioDePagos(Prestamo prestamo)
         {
+            // Fórmulas:
+            // 1. Pago Mensual: PMT = P * i_m / (1 - (1 + i_m)^-n)
+            // 2. Interés: I_t = S_{t-1} * i_m
+            // 3. Principal: Principal = PMT - I_t
+            // 4. Saldo: S_t = S_{t-1} - Principal
+
             List<Pago> calendario = new List<Pago>();
             decimal tasaInteresMensual = prestamo.TasaInteresAnual / 12 / 100;
             decimal pagoMensual = prestamo.Monto * tasaInteresMensual / (1 - (decimal)Math.Pow((double)(1 + tasaInteresMensual), -prestamo.NumeroPeriodos));
@@ -135,7 +196,5 @@ namespace Models
 
             return calendario;
         }
-
-
     }
 }
